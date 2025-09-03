ccb7dc711e868ab4495eeb2cf6d8421521c902bd
Benny Guardiola <bguardiola86@gmail.com>
2025-09-03 15:22:22 -0700

    uploading Hello code

---
+++ Hello.py
@@ -0,0 +1,8 @@
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# output = "hello, " + first_name + " " + last_name
+# output = "hello, {} {}" .format(first_name, last_name)
+# output = "hello, {0} {1}" .format(first_name, last_name)
+output = f"hello, {first_name} {last_name}"
+print(output)
