5fb04ee73f8ac31a7b7a18c155d032aac30865cb
Benny Guardiola <bguardiola86@gmail.com>
2025-09-04 11:06:50 -0700

    Added Mr. Ms. and Mrs. to hello.py
    Added more code that will make it so that if they leave the first name blank it will prompt asking if it's Mr., Mrs. or Ms. from there it will add Mr., Ms., Mrs. To the last name. 
    
    In the event that neither is entered then it will default to friend.

--- Hello.py
+++ Hello.py
@@ -1,8 +1,18 @@
 first_name = input("Enter your first name: ")
 last_name = input("Enter your last name: ")
 
-# output = "hello, " + first_name + " " + last_name
-# output = "hello, {} {}" .format(first_name, last_name)
-# output = "hello, {0} {1}" .format(first_name, last_name)
+if not first_name:  # If no first name was entered
+    # Try to guess based on last name (not reliable) or just ask
+    gender = input("Is this Mr. or Mrs.? ").strip().lower()
+    if gender in ["mr"or "mr.", "mister"]:
+        first_name = "Mr."
+    elif gender in ["ms", "ms."]:
+        first_name = "Ms."
+    elif gender in ["mrs.", "mrs"]:
+        first_name = "Mrs."
+    else:
+        first_name = "Friend"  # fallback if unclear
+
 output = f"hello, {first_name} {last_name}"
 print(output)
+
